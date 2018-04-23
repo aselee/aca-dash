dd856f2db653b56a935c8ee7c54272c35e51d5e9
Andrew <32229265+aselee@users.noreply.github.com>
2018-04-23 16:55:42 -0500

    just missing sort

--- index.js
+++ index.js
@@ -11,8 +11,19 @@
 //iteratee is a function that must return something, capture whatever it returns in a variable
 //add the returned value from iteratee tp myNewArray
 //after looping, return  myNewArray
-function map(array, iteratee){
+const arr1 = [5,12,3,10,22,18,7,2]
 
+const map = (arr1, iteratee) => {
+    // create varible with empty array
+    var myNewArray = [];
+    // loop array
+    // let i equals 0; i is less the array, the loop will continue to run 
+    for(let i = 0; i<arr1.length; i++) {
+    // use the empty variable to push each loop and call iteratee() to pass the value in the array
+        myNewArray.push(iteratee(arr1[i]));
+    }
+    // return
+    return myNewArray;
 }
 
 //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
@@ -24,9 +35,20 @@ function map(array, iteratee){
 //     passing in the item from the current loop
 //iteratee will return true or false, if true add the item to myNewArray else do not
 //after looping, return myNewArray
-function filter(array, iteratee){
-
+const filter = (arr1, iteratee) => {
+    // create empty variable
+    const newerArray = [];
+    // loop arr1 and call iteratee for each value in the array
+    for(let i = 0; i<arr1.length; i++) {
+    // use if statement to determine if iteratee will reuturn T/F
+        if(iteratee(arr1[i])) {
+            // to push value if true to empty array
+            newerArray.push(array[i]);
+        }
+    }
+    return newerArray;
 }
+
 
 //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
 //create a function called `find`, it should take 2 parameters `theArray` and `fnc`
@@ -34,18 +56,27 @@ function filter(array, iteratee){
 //     passing in the item from the current loop
 //fnc will return true or false, if true return the item 
 //after looping, return null
-function find(theArray, fnc){
-
+const find = (theArray, fnc) => {
+    // create for loop
+    for(let i = 0; i<theArray.length; i++) {
+        // create if statement for fnc if fnc will return T/F
+        if(fnc(theArray[i])) {
+            return theArray[i];
+        }
+    }
 }
 
 
 //return the last item in theArray
-function findLast(theArray){
-
+const findLast = (theArray) => {
+    // using theArray.length - 1; to find the last index in the array
+    return theArray[theArray.length - 1];
 }
 
 //return the first element of the array
-function head(theArray){
+const head = (theArray) => {
+    // the "0" index means the first # in the array
+    return theArray[0]
 
 }
 
@@ -53,7 +84,17 @@ function head(theArray){
 //loop theArray in reverse order
 //add the item from each loop to the new array
 //return the new array
-function reverse(theArray){
+const reverse = (theArray) => {
+    // create empty array
+    const newArr2 =[];
+    // use for loop to add item to the empty array
+    // using .length - 1, which corresponds to the last index
+    // As long as i is greater than or equals to 0, the loop will continue
+    for(let i = 0; theArray.length - 1; i >= 0; i--) {
+        // using push function to push the last index to return as first
+        newArr2.push(theArray[i]);
+    }
+    return newArr2;
 
 }
 
@@ -61,8 +102,15 @@ function reverse(theArray){
 //loop theArray
 //add the item from each loop to the new array except the first item
 //return the new array
-function tail(theArray){
-
+const tail = (theArray) => {
+    const newArr3 = [];
+    // creating for loop to add item to new array EXCEPT the first item
+    // let i equals 1; if i(1) is less than the array,
+    for(let i = 1; i < theArray.length; i++) {
+        // push
+        newArr3.push(theArray[i]);
+    }
+    return newArr3;
 }
 
 //implement the most basic sorting algorithm there is
@@ -75,7 +123,7 @@ function tail(theArray){
 //if a swap is done set it to true
 //after each for loop check the variable, if true, continue the while loop
 //if false return theArray
-function sort(theArray){
+const sort = (theArray) => {
 
 }
 
